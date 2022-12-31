7a620043f3586986c129ef44ff15a226165c51f5
aridsm <aniemorelanie@gmail.com>
2022-12-31 11:38:20 -0300

    added link to github

--- src/components/AccountSection/AccountData.tsx
+++ src/components/AccountSection/AccountData.tsx
@@ -33,7 +33,7 @@ const AccountData: React.FC = () => {
         <TasksDone />
         <DeleteTasks />
         <a
-          href="/"
+          href="https://github.com/aridsm"
           className="mt-4 bg-rose-100 p-2 rounded-md text-rose-600 text-center transition hover:bg-rose-200 dark:bg-slate-700/[.3] dark:text-slate-200"
         >
           Projected by Ariane Morelato
